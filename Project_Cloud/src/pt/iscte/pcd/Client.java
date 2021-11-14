02df365c7dfa2dca08eb99938f4f61cbe35aab69
glrss-iscte <glrss@iscte-iul.pt>
2021-11-14 17:52:27 +0000

    Client.java

--- Project_Cloud/src/pt/iscte/pcd/Client.java
+++ Project_Cloud/src/pt/iscte/pcd/Client.java
@@ -2,19 +2,27 @@ package pt.iscte.pcd;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
-public class Client{
+public class Client implements ActionListener {
     private ObjectInputStream in;
     private ObjectOutputStream out;
+    JButton button;
+    JTextField text;
+    JTextField text2;
+    String S1;
+    String S2;
 
     public Client(String addressText, int port){
+        /*
         System.err.println("Connecting to:"+ addressText +":"+port);
-        //connect to node
+        //connect to node - (REMOVE THIS)
         try {
             InetAddress address = InetAddress.getByName(addressText);
             Socket s = new Socket(address,port);
@@ -24,6 +32,7 @@ public class Client{
         } catch (IOException e) {
             e.printStackTrace();
         }
+         */
     }
 
     public void UI(){
@@ -33,9 +42,10 @@ public class Client{
 
         JTextArea text_x = new JTextArea("Posiçao a consultar:");
         JTextArea text_y = new JTextArea("Comprimento:");
-        JButton button = new JButton("Consultar");
-        JTextField text = new JTextField();
-        JTextField text2 = new JTextField();
+        this.button = new JButton("Consultar");
+        this.button.addActionListener(this);
+        this.text = new JTextField();
+        this.text2 = new JTextField();
         JTextArea textArea = new JTextArea("Respostas aparecerao aqui...");
         text_x.setEditable(false);
         text_y.setEditable(false);
@@ -53,5 +63,17 @@ public class Client{
         frame.pack();
         frame.setVisible(true);
         frame.setSize(830,200);
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == this.button){
+           S1 = text.getText();
+           S2 = text2.getText();
+        }
+        System.out.println(S1);
+        System.out.println(S2);
     }
 }
